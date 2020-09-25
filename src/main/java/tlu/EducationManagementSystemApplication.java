85ba68d7a6f49d4f5e4706f4ed5adf41259f9129
xuanngoc <bx.ngoc.99@gmail.com>
2020-09-25 22:03:28 +0700

    init

---
+++ src/main/java/tlu/EducationManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package tlu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducationManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tlu/EducationManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package tlu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
