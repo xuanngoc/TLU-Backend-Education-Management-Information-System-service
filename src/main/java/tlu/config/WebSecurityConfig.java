334ba2b48bfb01a3c56674622753ef90400d69de
xuanngoc <bx.ngoc.99@gmail.com>
2020-10-18 10:15:57 +0700

    fix ocrs

--- src/main/java/tlu/config/WebSecurityConfig.java
+++ src/main/java/tlu/config/WebSecurityConfig.java
@@ -45,14 +45,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
         auth.userDetailsService(userService).passwordEncoder(passwordEncoder());
     }
     
-    @Override
+    @Override		
     protected void configure(HttpSecurity http) throws Exception {
-        http
+        http	
+        	.csrf()
+        	.disable()
+        	
                 .authorizeRequests()
                     .antMatchers("/api/v1/login").permitAll() // Cho phép tất cả mọi người truy cập vào địa chỉ này
                     .anyRequest().authenticated(); // Tất cả các request khác đều cần phải xác thực mới được truy cập
 
         // Thêm một lớp Filter kiểm tra jwt
         http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+        http.cors();
     }
 }
