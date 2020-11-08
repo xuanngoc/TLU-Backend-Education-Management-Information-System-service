39fcbbd5eb12c9e62bdf76138456d4628001fe0b
Bui Xuan Ngoc <ngocbui.se@gmail.com>
2020-11-08 10:19:26 +0700

    Update WebSecurityConfig.java

--- src/main/java/tlu/config/WebSecurityConfig.java
+++ src/main/java/tlu/config/WebSecurityConfig.java
@@ -52,6 +52,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
         	.disable()
         	
                 .authorizeRequests()
+		    .antMatchers("/", "/api/v1/mon-hoc")
+					.permitAll()
                     .antMatchers("/api/v1/login").permitAll() // Cho phép tất cả mọi người truy cập vào địa chỉ này
                     .anyRequest().authenticated(); // Tất cả các request khác đều cần phải xác thực mới được truy cập
 
